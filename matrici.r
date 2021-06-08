0c3c9b42767c0626d8545e1d35fffa262c99c8b8
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2021-06-08 15:52:54 +0200

    Update matrici.r

--- matrici.r
+++ matrici.r
@@ -1,6 +1,15 @@
- wd<- setwd("C:/Thesis")
- library(rgdal)
- islands <- readOGR(paste0("Isole mediterraneo 0.5 Ha.gpkg"))
- coastline <- readOGR(paste0("coastline_eur_afr.gpkg"))
- 
+wd<- setwd("C:/Thesis")
+library(raster)
+library(sf)
+list.files(wd)
+islands <- st_read("isole_medit_0.5Ha.gpkg" )
+islands1 <- st_read("1º.gpkg")
+islands<- st_read("microgruppo perprova.gpkg") #MULTIPOLYGONS
+grid <- st_make_grid(islands,cellsize = 1000,what="centers")
+grid <- st_intersection(grid, islands)
+plot(grid)
+
+coastline <-st_read("coastlines_eurafr.gpkg") #MULTILYNESTRING
+# gia tiene la CRS uglae
+
 distance <- st_distance( islands, coastline, by_element= TRUE) #nopeeeeee
